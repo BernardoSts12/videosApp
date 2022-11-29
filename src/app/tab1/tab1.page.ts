7eccc8c4c51695140a0723f8c897dbd8820826bf
Bernardo Santos <bernardosilvasilver@gmail.com>
2022-11-29 19:32:57 -0300

    adicionado a interface e array de filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilmes } from '../models/iFilmes.model';
 
 @Component({
   selector: 'app-tab1',
@@ -9,9 +10,26 @@ import { ToastController } from '@ionic/angular';
 })
 
 export class Tab1Page {
-  handlerMessage = '';
-  roleMessage = '';
 
+  titulo = 'Vídeos App';
+  listaFilmes: IFilmes[] = [
+    {
+      nome: 'The Walking Dead',
+      lancamento: '12/10/2001',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xf9wuDcqlUPWABZNeDKPbZUjWx0.jpg',
+      generos: ['Ação','Terror']
+    },
+    {
+      nome: 'Adão Negro',
+      lancamento: '12/10/2001',
+      duracao: '1h 47m',
+      classificacao: 80,
+      cartaz : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9z256FFPDsL7kSVJ9oyLELaN1ph.jpg',
+      generos: ['Ação','Fantasia','Aventura', 'Ficção']
+    }
+  ];
 
   constructor(private alertController: AlertController, private toastController: ToastController) {}
 
